ac08519c3cbd8dbfe56ab66dd210d2dda5a014d2
= <luke@lukezsmith.com>
2021-12-30 00:31:39 +0000

    pfp mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,69 @@ import { DownloadBtn } from "../components/DownloadBtn";
 import { Gif } from "../components/Gif";
 import ClipLoader from "react-spinners/ClipLoader";
 import Switch from "react-switch";
+import captureVideoFrame from "capture-video-frame";
+import Cropper from "cropperjs";
+
+// import extractVideoFrames from 'extract-video-frames';
 
 const ffmpeg = createFFmpeg({ log: true });
 
 const ipfsURL =
   "https://ipfs.io/ipfs/QmUZxB4PmiwWa524ScscBsXiMhnLHAsB6N9JHtM4jv5Ppu/";
+
+async function extractFramesFromVideo(videoUrl: string, fps = 30) {
+  return new Promise(async (resolve) => {
+    // fully download it first (no buffering):
+    let videoBlob = await fetch(videoUrl).then((r) => r.blob());
+    let videoObjectUrl = URL.createObjectURL(videoBlob);
+    let video = document.createElement("video");
+
+    let seekResolve: { (): void; (value: unknown): void };
+    video.addEventListener("seeked", async function () {
+      if (seekResolve) seekResolve();
+    });
+
+    video.src = videoObjectUrl;
+
+    // workaround chromium metadata bug (https://stackoverflow.com/q/38062864/993683)
+    while (
+      (video.duration === Infinity || isNaN(video.duration)) &&
+      video.readyState < 2
+    ) {
+      await new Promise((r) => setTimeout(r, 500));
+      video.currentTime = 10000000 * Math.random();
+    }
+    let duration = video.duration / 3;
+
+    let canvas = document.createElement("canvas");
+    let context = canvas.getContext("2d");
+    let [w, h] = [video.videoWidth, video.videoHeight];
+    canvas.width = w;
+    canvas.height = h;
+
+    // let frames = [];
+    let interval = 1 / fps;
+    let currentTime = 0;
+
+    // while (currentTime < duration) {
+    //   video.currentTime = currentTime;
+    //   await new Promise((r) => (seekResolve = r));
+
+    //   context!.drawImage(video, 0, 0, w, h);
+    //   let base64ImageData = canvas.toDataURL();
+    //   frames.push(base64ImageData);
+
+    //   currentTime += interval;
+    // }
+    video.currentTime = 22.5 * (1/fps);
+    await new Promise((r) => (seekResolve = r));
+    context!.drawImage(video, 0, 0, w, h);
+    let base64ImageData = canvas.toDataURL();
+    let frame = base64ImageData;
+    resolve(frame)
+    // resolve(frames);
+  });
+}
 
 const Home: NextPage = () => {
   const [ready, setReady] = useState(false);
@@ -20,6 +78,8 @@ const Home: NextPage = () => {
   const [gifDuration, setGifDuration] = useState("2.2");
   const [reversed, setReversed] = useState(false);
   const [flipped, setFlipped] = useState(false);
+  const [isMire, setisMireable] = useState(false);
+  const [headTrait, setHeadTrait] = useState("");
 
   const load = async () => {
     await ffmpeg.load();
@@ -28,7 +88,6 @@ const Home: NextPage = () => {
 
   useEffect(() => {
     load();
-    // console.log(videoURL)
   }, []);
 
   const getVideoURL = async (e: string) => {
@@ -58,6 +117,81 @@ const Home: NextPage = () => {
         let index = data.attributes.findIndex(
           (x: { trait_type: string }) => x.trait_type === "Species"
         );
+        if (data.attributes[index].value === "mire-able") {
+          setisMireable(true);
+
+          // check for head traits
+          if (
+            data.attributes.some(
+              (x: { value: string }) => x.value === "smoking pipe"
+            )
+          ) {
+            setHeadTrait("pipe");
+          } else if (
+            data.attributes.some(
+              (x: { value: string }) => x.value === "lodged cigarette"
+            )
+          ) {
+            setHeadTrait("cig");
+          } else if (
+            data.attributes.some(
+              (x: { value: string }) => x.value === "lodged sawblade"
+            )
+          ) {
+            setHeadTrait("saw");
+          } else if (
+            data.attributes.some(
+              (x: { value: string }) => x.value === "lodged bitcoin"
+            )
+          ) {
+            setHeadTrait("btc");
+          } else if (
+            data.attributes.some(
+              (x: { value: string }) => x.value === "lodged ether"
+            )
+          ) {
+            setHeadTrait("eth");
+          } else {
+            setHeadTrait("");
+          }
+        } else {
+          setisMireable(false);
+          // setHeadTrait("");
+                    // check for head traits
+                    if (
+                      data.attributes.some(
+                        (x: { value: string }) => x.value.includes("leaf")
+                      )
+                    ) {
+                      setHeadTrait("leaf");
+                    } else if (
+                      data.attributes.some(
+                        (x: { value: string }) => x.value === "goggles"
+                      )
+                    ) {
+                      setHeadTrait("goggles");
+                    } else if (
+                      data.attributes.some(
+                        (x: { value: string }) => x.value === "lodged sushi knife"
+                      )
+                    ) {
+                      setHeadTrait("knife");
+                    } else if (
+                      data.attributes.some(
+                        (x: { value: string }) => x.value === "top hat"
+                      )
+                    ) {
+                      setHeadTrait("top hat");
+                    } else if (
+                      data.attributes.some(
+                        (x: { value: string }) => x.value === "aviators"
+                      )
+                    ) {
+                      setHeadTrait("aviators");
+                    } else {
+                      setHeadTrait("");
+                    }
+        }
         setGifDuration(
           data.attributes[index].value === "mire-able" ? "2" : "2.2"
         );
@@ -73,24 +207,22 @@ const Home: NextPage = () => {
     let gifConfig = "";
 
     if (!reversed) {
-      if(!flipped){
+      if (!flipped) {
         gifConfig =
           "fps=45,scale=640:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse";
-      }else{
+      } else {
         gifConfig =
-        "hflip,fps=45,scale=640:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse";
+          "hflip,fps=45,scale=640:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse";
       }
     } else {
-      if (!flipped){
+      if (!flipped) {
         gifConfig =
           "fps=45,reverse, scale=640:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse";
-      }else{
+      } else {
         gifConfig =
-        "hflip,reverse, fps=45,scale=640:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse";
-
+          "hflip,reverse, fps=45,scale=640:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse";
       }
     }
-  
 
     // Run the FFmpeg command-line tool, converting
     // the .mp4 into .gif file
@@ -112,8 +244,108 @@ const Home: NextPage = () => {
     setGif(url);
   };
 
+  const generatePFP = async () => {
+    setLoading(true);
+
+    let frame;
+    var temp = new Image();
+    temp.className = "pfp-hidden";
+  
+    if (isMire){
+      frame = captureVideoFrame("vid", "png", 1);
+      temp.src = frame.dataUri;
+    
+    }else{
+      frame = await extractFramesFromVideo(videoURL);
+      temp.src = frame;
+
+    }
+    setTimeout(() => {
+      var canvas = document.createElement("canvas") as HTMLCanvasElement;
+      canvas.width = 500;
+      canvas.height = 500;
+      var canvasContext = canvas.getContext("2d");
+
+      canvasContext!.translate(500, 0);
+      canvasContext!.scale(-1, 1);
+
+      // MIRE SETTINGS
+      if (isMire) {
+        switch (headTrait) {
+          case "pipe":
+            // 5792
+            canvasContext!.scale(1.4, 1.4);
+            canvasContext!.drawImage(temp, -35, -10, 500, 500);
+            break;
+          case "cig":
+            // 235
+            canvasContext!.scale(1.5, 1.5);
+            canvasContext!.drawImage(temp, -70, -10, 500, 500);
+            break;
+          case "saw":
+            // 8181
+            canvasContext!.scale(1.45, 1.45);
+            canvasContext!.drawImage(temp, -70, -10, 500, 500);
+            break;
+          case "btc":
+            // 1405
+            canvasContext!.scale(1.5, 1.5);
+            canvasContext!.drawImage(temp, -75, -10, 500, 500);
+            break;
+          case "eth":
+            // 4087
+            canvasContext!.scale(1.45, 1.45);
+            canvasContext!.drawImage(temp, -85, -10, 500, 500);
+            break;
+          default:
+            canvasContext!.scale(1.65, 1.65);
+            canvasContext!.drawImage(temp, -85, -10, 500, 500);
+            break;
+        }
+      } else {
+        // BOG SETTINGS
+        switch (headTrait) {
+          case "leaf":
+            // 8856
+            canvasContext!.scale(1.3, 1.3);
+            canvasContext!.drawImage(temp, -75, -20, 500, 500);
+            break;
+          case "goggles":
+            // 5928
+            canvasContext!.scale(1.25, 1.25);
+            canvasContext!.drawImage(temp, -65, -5, 500, 500);
+            break;
+          case "knife":
+            // 4747
+            canvasContext!.scale(1.2, 1.2);
+            canvasContext!.drawImage(temp, -55, 10, 500, 500);
+            break;
+          case "top hat":
+            // 9982
+            canvasContext!.scale(1.2, 1.2);
+            canvasContext!.drawImage(temp, -55, 10, 500, 500);
+            break;
+          case "aviators":
+            // 1851
+            canvasContext!.scale(1.25, 1.25);
+            canvasContext!.drawImage(temp, -65, -5, 500, 500);
+            break;
+          default:
+            canvasContext!.scale(1.3, 1.3);
+            canvasContext!.drawImage(temp, -75, -40, 500, 500);
+            break;
+        }
+
+      }
+      setLoading(false);
+      setGif(canvas.toDataURL("image/png"));
+      canvas.remove();
+      temp.remove();
+      return;
+    }, 1000);
+  };
+
   const download = (e: React.MouseEvent, type: string) => {
-    console.log((e.target as HTMLAnchorElement).href);
     fetch((e.target as HTMLAnchorElement).href, {
       method: "GET",
       headers: {},
@@ -180,17 +412,34 @@ const Home: NextPage = () => {
           {videoURL !== "" ? (
             <div>
               <div className="lg:my-10">
-                <video controls crossOrigin="anonymous" src={videoURL} />
+                <video
+                  id="vid"
+                  controls
+                  crossOrigin="anonymous"
+                  src={videoURL}
+                />
               </div>
-              <div className=" my-3 lg:my-0 ">
-                {/* video download btn */}
-                {/* <DownloadBtn url={videoURL} type="video" download={download} />{" "} */}
-                <a
-                  className="cursor-pointer p-3 bg-gray-300 rounded-md hover:bg-gray-400"
-                  onClick={convertToGif}
-                >
-                  Convert to GIF
-                </a>
+              <div className="grid grid-cols-2">
+                <div className=" my-3 lg:my-0 ">
+                  {/* video download btn */}
+                  {/* <DownloadBtn url={videoURL} type="video" download={download} />{" "} */}
+                  <a
+                    className="cursor-pointer p-3 bg-gray-300 rounded-md hover:bg-gray-400"
+                    onClick={convertToGif}
+                  >
+                    Convert to GIF
+                  </a>
+                </div>
+                <div className=" my-3 lg:my-0 ">
+                  {/* video download btn */}
+                  {/* <DownloadBtn url={videoURL} type="video" download={download} />{" "} */}
+                  <a
+                    className="cursor-pointer p-3 bg-gray-300 rounded-md hover:bg-gray-400"
+                    onClick={generatePFP}
+                  >
+                    Generate PFP
+                  </a>
+                </div>
               </div>
               <label className="flex align-center justify-center lg:mt-10">
                 <span>Moonwalking shroom?</span>
